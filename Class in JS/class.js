8f01addbbb32252b56a82bec5c27968be9cd1d69
Dhaval <joshidhaval2002@gmail.com>
2023-09-20 23:58:32 +0530

    Classes in JS ,fomrat containing

--- Class in JS/class.js
+++ Class in JS/class.js
@@ -11,3 +11,11 @@ function Vehicle(name,color,wheels){
 let vehicle1=new Vehicle('BMW','Red',4) //new keyword is for creating objects
 console.log(vehicle1)   //this will return objects with getdetails function also inside it
 vehicle1.getDetails() 
+
+//Classes in JS
+class VehicleCl{
+    // contains
+    //1.properties
+    //2.Constructor
+    //3.Methods
+}
